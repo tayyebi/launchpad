38d2166c84c30fe7849ab6eb185ffb5232e1732a
tayyebi <smile@tyyi.net>
2026-06-11 17:29:23 +0400

    Keep screen on while using the app via wakelock_plus

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
 import 'app.dart';
 import 'core/database/database.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await AppDatabase.instance.initialize();
+  await WakelockPlus.enable();
   runApp(const LaunchpadApp());
 }
