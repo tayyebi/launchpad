415a1a261a76238a20066774ef131ba8059ede9c
tayyebi <smile@tyyi.net>
2026-06-16 18:06:05 +0400

    Fix google_fonts API - use vazirmatnTextTheme

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -2,16 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
-  static ThemeData get dark => GoogleFonts.vazirmatn(
-        ThemeData(
-          useMaterial3: true,
-          brightness: Brightness.dark,
-          colorSchemeSeed: const Color(0xFF00BCD4),
-          scaffoldBackgroundColor: const Color(0xFF121212),
-          appBarTheme: const AppBarTheme(
-            centerTitle: true,
-            elevation: 0,
-          ),
-        ),
-      );
+  static ThemeData get dark {
+    final base = ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.dark,
+      colorSchemeSeed: const Color(0xFF00BCD4),
+      scaffoldBackgroundColor: const Color(0xFF121212),
+      appBarTheme: const AppBarTheme(
+        centerTitle: true,
+        elevation: 0,
+      ),
+    );
+    return base.copyWith(
+      textTheme: GoogleFonts.vazirmatnTextTheme(base.textTheme),
+    );
+  }
 }
